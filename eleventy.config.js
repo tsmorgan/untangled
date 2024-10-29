412b3704bfd8b58ed37e4fd1cdeb6524925bd93d
Tom Morgan <tsmorgan@gmail.com>
2024-10-29 13:52:55 +0000

    updates

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import { DateTime } from "luxon";
+import striptags from "striptags";
 
 export default function (eleventyConfig) {
 
@@ -10,6 +11,8 @@ export default function (eleventyConfig) {
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
+    eleventyConfig.addFilter("excerpt", (content) => extractExcerpt(content));   
+
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toFormat("d LLLL yyyy");
     });
@@ -20,3 +23,18 @@ export default function (eleventyConfig) {
         },
     };
 };
+
+
+function extractExcerpt(content) {    
+  
+    let excerpt = null;
+    // const content = article.templateContent;
+  
+    excerpt = striptags(content)
+      .substring(0, 150) // Cap at 200 characters
+      .replace(/^\\s+|\\s+$|\\s+(?=\\s)|\n/g, " ")
+      .trim()
+      .concat("...");
+
+    return excerpt;
+  }
