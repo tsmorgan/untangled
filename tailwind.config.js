2a7304af7145767f01a3b717f2a330c73417c49c
Tom Morgan <tsmorgan@gmail.com>
2024-10-29 17:01:18 +0000

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./_src/**/*.{html,njk}'],
-    theme: {      
-      extend: {
-        colors: {
-          "untangled" : "rgb(216, 223, 215)",
-          "untangled-dark" : "rgb(45, 77, 68)",
-        },
+  content: ['./_src/**/*.{html,njk}'],
+  theme: {
+    extend: {
+      colors: {
+        "untangled": "rgb(216, 223, 215)",
+        "untangled-dark": "rgb(45, 77, 68)",
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
